054be7aebbe174f9517308d74b9a0375068f01de
Nahun Menem <86641118+NahunMenem@users.noreply.github.com>
2025-09-02 09:34:09 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        onyx: '#1C1C1B',
+        walnut: '#6A5D52',
+        ash: '#979086',
+        greige: '#B7AC9B',
+        stucco: '#E2E2DE',
+      },
+    },
+  },
+  plugins: [],
+}
